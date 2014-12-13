6150d30bed8f9f57d6b227f907d470b922ae76e1
FullContactCoding <tim@fullcontactcoding.com>
2014-12-13 05:47:46 +0100

    added function to convert decimal to fractions

--- oddsconverter.php
+++ oddsconverter.php
@@ -1,9 +1,53 @@
 <!-- create dropdown box -->
-<form method="post" id="oddsDropdown" action="oddsconverterBack.php">
-    <select name="odds type" onchange="document.getElementById('oddsDropdown').submit();">
+<form action="oddsconverter.php" method="post">
+    <select name="oddstype" onchange="this.form.submit();">
+        <option value="choose">Choose oddstype</option>
         <option value="decimal">decimal</option>
         <option value="fractional">fractional</option>
         <option value="moneyline">moneyline</option>
     </select>
 </form>
-<div>3.50</div>
+<?php
+//store chosen value in a variable
+$oddstype = $_POST['oddstype'];
+$odds = 3;
+
+
+
+// convert decimal to fractions
+
+if ($oddstype == "fractional") {
+    function dec2frac($dec)
+    {
+
+        $decBase = --$dec;
+
+        $div = 1;
+
+        do {
+
+            $div++;
+
+            $dec = $decBase * $div;
+
+        } while (intval($dec) != $dec);
+
+        if ($dec % $div == 0) {
+            $dec = $dec / $div;
+            $div = $div / $div;
+        }
+
+        return $dec . '/' . $div;
+
+    }
+    return $odds = dec2frac($odds);
+}
+
+
+
+
+?>
+<div><?php echo $oddstype; ?></div>
+
+<div><?php echo $odds; ?></div>
+
